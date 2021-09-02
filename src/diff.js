2d63afb6a91e46611a9be44e4af1d7ad4a151393
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2021-09-03 05:59:55 +0700

    fix error

--- src/diff.js
+++ src/diff.js
@@ -1,9 +1,9 @@
-import DateTime from './luxon.js';
+import {DateTime} from './luxon.js';
 
 export const diffDate = ( firstDate, secondDate ) => {
     const   firstDateDT = DateTime.fromISO( firstDate ),
             secondDateDT = DateTime.fromISO( secondDate );
 
-    return secondDateDT.diff( firstDate, ['years', 'months', 'days'] );
+    return secondDateDT.diff( firstDateDT, ['years', 'months', 'days'] );
 
 }
